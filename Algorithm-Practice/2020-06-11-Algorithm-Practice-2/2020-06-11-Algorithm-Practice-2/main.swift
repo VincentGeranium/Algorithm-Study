4e7b2ce03b59878fd54e4702dd71e65d2bc717c5
김광준 <kwangjun3952@gmail.com>
2020-06-11 22:28:33 +0900

    200611 백준 알고리즘 문제(10988, A * B) 풀이 코드

---
+++ Algorithm-Practice/2020-06-11-Algorithm-Practice-2/2020-06-11-Algorithm-Practice-2/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  2020-06-11-Algorithm-Practice-2
+//
+//  Created by 김광준 on 2020/06/11.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+import Foundation
+
+let a = readLine()
+
+if let sol = a {
+    let array = sol.components(separatedBy: " ")
+    
+    if let first = Int(array[0]), let second = Int(array[1]) {
+        print(first * second)
+    }
+}
+
