8df6a2caee60132e352fac6f0c075e232c4ce133
김광준 <kwangjun3952@gmail.com>
2020-10-09 02:39:15 +0900

    201009 백준 - N찍기

---
+++ Algorithm-Practice/2020-10-09-Algorithm-Practice-1/2020-10-09-Algorithm-Practice-1/main.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  2020-10-09-Algorithm-Practice-1
+//
+//  Created by 김광준 on 2020/10/09.
+//  Copyright © 2020 VincentGeranium. All rights reserved.
+//
+
+
+// 백준 2741번 문제 - N찍기
+import Foundation
+
+let a = readLine()
+
+if let a = a {
+    if let sol = Int(a) {
+        for i in 1...sol {
+            print(i)
+        }
+    }
+}
+
+
