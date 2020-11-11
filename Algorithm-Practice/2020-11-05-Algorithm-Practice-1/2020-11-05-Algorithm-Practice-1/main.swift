11c1f8e86be525ae623c18755a41fcdb061502b4
김광준 <kwangjun3952@gmail.com>
2020-11-11 14:13:04 +0900

    백준 온라인 저지 문제 풀이

--- Algorithm-Practice/2020-11-05-Algorithm-Practice-1/2020-11-05-Algorithm-Practice-1/main.swift
+++ Algorithm-Practice/2020-11-05-Algorithm-Practice-1/2020-11-05-Algorithm-Practice-1/main.swift
@@ -7,55 +7,41 @@
 
 import Foundation
 
-let a = readLine()
-let b  = readLine()
-
-
-if let stringA = a, let stringB = b {
-    let arrayOne = stringA.components(separatedBy: " ")
-    let arrayTwo = stringB.components(separatedBy: " ")
-    
-    if let N = Int(arrayOne[0]), let X = Int(arrayOne[1]) {
-        if N >= 1 && X <= 10000 {
-            print("N : \(N)")
-            print("X : \(X)")
-            
-            let arr = arrayTwo
-            var result: [Int] = []
-            
-            if arr.count == N {
-                for i in arr {
-                    if let intI = Int(i) {
-                        if intI < X {
-                            result.append(intI)
-                        }
-                    }
-                }
-            }
-            print(result)
-        }
-    }
-}
-
-
-//for i in arr {
-//    if let tempI = Int(i) {
-//        tempArr.append(tempI)
-//    }
-//}
-
 //let a = readLine()
+//let b  = readLine()
 //
-//if let sol = a {
-//    let array = sol.components(separatedBy: " ")
 //
-//    if let first = Int(array[0]), let second = Int(array[1]) {
-//        if first > second {
-//            print(">")
-//        } else if first < second {
-//            print("<")
-//        } else if first == second {
-//            print("==")
+//if let stringA = a, let stringB = b {
+//    let arrayOne = stringA.components(separatedBy: " ")
+//    let arrayTwo = stringB.components(separatedBy: " ")
+//
+//    if let N = Int(arrayOne[0]), let X = Int(arrayOne[1]) {
+//        if N >= 1 && X <= 10000 {
+//            print("N : \(N)")
+//            print("X : \(X)")
+//
+//            let arr = arrayTwo
+//            var result: [Int] = []
+//
+//            if arr.count == N {
+//                for i in arr {
+//                    if let intI = Int(i) {
+//                        if intI < X {
+//                            result.append(intI)
+//                        }
+//                    }
+//                }
+//            }
+//            print(result)
 //        }
 //    }
 //}
+
+/// https://0urtrees.tistory.com/178 참고함.
+
+let arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+let X = arr[1]
+//print(X)
+
+let arrA = readLine()!.split(separator: " ").map { Int(String($0))! }.filter { $0 < X }.map { "\($0)"}.joined(separator: " ")
+print(arrA)
